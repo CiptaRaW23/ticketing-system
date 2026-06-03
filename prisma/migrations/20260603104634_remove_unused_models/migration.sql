df3d93a686771e3e412b259c3a840d5d0951d792
cipta <crwijaya15@gmail.com>
2026-06-03 17:48:52 +0700

    Menghapus model yang tidak perlu di prisma

---
+++ prisma/migrations/20260603104634_remove_unused_models/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the `admin_activity_logs` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `system_settings` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "admin_activity_logs";
+
+-- DropTable
+DROP TABLE "system_settings";
